7b2a79d8260a5b15b3f443f734ab98cf16acdb82
Kristina Jeršova <kristinajershova5@gmail.com>
2026-05-09 22:52:50 +0300

    change

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,5 +1,8 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+const prisma = new PrismaClient({
+    adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL as string,
+    }),
+});
 export default prisma;
