3d1388cac2ec4bbf9b068675269782a8be9670a7
Kristina Jeršova <kristinajershova5@gmail.com>
2026-05-10 21:29:56 +0300

    Added authors page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+
 import BooksPage from "../src/pages/BookPage";
 import BookDetailPage from "../src/pages/BookDetailPage";
 import BookCreatePage from "../src/pages/BookCreatePage";
 import BookEditPage from "../src/pages/BookEditPage";
+import AuthorsPage from "../src/pages/AuthorsPage";
 
 export default function App() {
   return (
@@ -11,11 +13,25 @@ export default function App() {
 
       <Route path="/books" element={<BooksPage />} />
 
-      <Route path="/books/new" element={<BookCreatePage />} />
+      <Route
+        path="/books/new"
+        element={<BookCreatePage />}
+      />
 
-      <Route path="/books/:id" element={<BookDetailPage />} />
+      <Route
+        path="/books/:id"
+        element={<BookDetailPage />}
+      />
 
-      <Route path="/books/:id/edit" element={<BookEditPage />} />
+      <Route
+        path="/books/:id/edit"
+        element={<BookEditPage />}
+      />
+
+      <Route
+        path="/authors"
+        element={<AuthorsPage />}
+      />
     </Routes>
   );
 }
