7062deb7f7a18f4e84669c35d141a5538e89f906
Kristina Jeršova <kristinajershova5@gmail.com>
2026-05-10 21:30:40 +0300

    Added authors and review delete routes

--- backend/src/routes/express/book.routes.ts
+++ backend/src/routes/express/book.routes.ts
@@ -3,6 +3,8 @@ import * as bookController from "../../controllers/book.controller";
 
 const router = Router();
 
+router.get("/authors", bookController.getAuthorsHandler);
+
 router.get("/books", bookController.getBooksHandler);
 router.get("/books/:bookId", bookController.getBookByIdHandler);
 router.post("/books", bookController.createBookHandler);
@@ -12,5 +14,7 @@ router.delete("/books/:bookId", bookController.deleteBookHandler);
 router.post("/books/:bookId/reviews", bookController.createReviewHandler);
 router.get("/books/:bookId/reviews", bookController.getReviewsByBookHandler);
 router.get("/books/:bookId/average-rating", bookController.getAverageRatingHandler);
+
+router.delete("/reviews/:reviewId", bookController.deleteReviewHandler);
 
 export default router;
