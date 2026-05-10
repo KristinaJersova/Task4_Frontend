e89b5c2123e390de09de54be390e2d29479a609b
Kristina Jeršova <kristinajershova5@gmail.com>
2026-05-10 21:08:02 +0300

    added app.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import BooksPage from "../src/pages/BookPage";
+import BookDetailPage from "../src/pages/BookDetailPage";
+import BookCreatePage from "../src/pages/BookCreatePage";
+import BookEditPage from "../src/pages/BookEditPage";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/books" />} />
+
+      <Route path="/books" element={<BooksPage />} />
+
+      <Route path="/books/new" element={<BookCreatePage />} />
+
+      <Route path="/books/:id" element={<BookDetailPage />} />
+
+      <Route path="/books/:id/edit" element={<BookEditPage />} />
+    </Routes>
+  );
+}
